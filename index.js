6c4f2e17fce4e730b669ed52b1ed253ee8980a2b
rodrydani <rodrydanielmir@gmail.com>
2022-10-08 20:36:41 -0300

    se corrigio la funcionalidad del menu hamburgesa

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const navToggle = document.querySelector(".nav-toogle");
+/*const navToggle = document.querySelector(".nav-toogle");
 const navMenu= document.querySelector(".nav-menu");
 
-navToggle.addEventListener ("click", () =>{
-    navMenu.classList.toggle("nav-menu-visible");
-});
+   
+        navToggle.addEventListener ("click", () =>{
+             
+            navMenu.classList.toggle("nav-menu-visible");
+        });*/
+
+        const navMenu = document.getElementById("nav-menu");
+        const navToggle= document.getElementById("nav-toogle");
+
+       navToggle.addEventListener("click",()=>{
+            if(navMenu.classList.contains("visible")){
+                navMenu.classList.remove("visible");
+            }else{
+                navMenu.classList.add("visible");
+            }
+           });
+
+    
+
